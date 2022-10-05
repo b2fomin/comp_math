34358bcbaa8f9a124ac73a0bd714645e7f9253dd
123 <fomin.nv@phystech.edu>
2022-10-05 21:59:52 +0300

    constructors and destructor

--- Polynomial.h
+++ Polynomial.h
@@ -1,1 +1,24 @@
 #pragma once
+#include<vector>
+
+template<typename T>
+class Polynomial
+{
+private:
+	std::vector<T> coeff;
+public:
+	Polynomial() = default;
+	Polynomial(std::vector<T>& coeff);
+	template<typename... Args>
+	Polynomial(Args... args);
+};
+
+template<typename T>
+Polynomial<T>::Polynomial(std::vector<T>& coeff) : coeff{ coeff } {};
+
+template<typename T>
+template<typename... Args>
+Polynomial<T>::Polynomial(Args... args)
+{
+
+}
