32fb140d93c86e1c0210128c1ea366762f86f3f7
123 <fomin.nv@phystech.edu>
2022-10-06 04:25:16 +0300

    rough_sol mathod and renaming sturm_root_number

--- eq_solving.h
+++ eq_solving.h
@@ -3,9 +3,10 @@
 #include"Polynomial.h"
 #include<vector>
 #include<numeric>
+#include<utility>
 
 template<typename T>
-int Sturm_root_number(Polynomial<T>& pol, double a, double b)
+std::size_t sturm_root_number(Polynomial<T>& pol, double a, double b)
 {
 	std::vector<Polynomial<T>> sturm_seq;
 	sturm_seq.push_back(pol);
@@ -32,5 +33,47 @@ int Sturm_root_number(Polynomial<T>& pol, double a, double b)
 		}
 	}
 
-	return result;
+	return std::abs(result);
+}
+
+template<typename T>
+std::vector<std::pair<double, double>> rough_sol(Polynomial<T>& pol)
+{
+	auto coeff = pol.coeff_();
+	for (auto& elem : coeff) elem = std::abs(elem);
+
+	auto left_total = coeff[0] / (coeff[0] + *std::max_element(coeff.begin() + 1, coeff.end()));
+	auto right_total = 1 + *std::max_element(coeff.begin(), coeff.end() - 1) / coeff[coeff.size() - 1];
+
+	auto total = sturm_root_number(pol, left_total, right_total);
+	std::vector<std::pair<double, double>> intervals{ total };
+
+	for (int i = 0; i < total; ++i)
+	{
+		double left, right;
+		if (i) left = intervals[i - 1].second + pow(10, -6);
+		else left = left_total;
+		right = right_total;
+
+		while (true)
+		{
+			auto middle = (left + right) / 2;
+			auto result = sturm_root_number(pol, left, middle);
+			if (result > 1)
+			{
+				right = middle;
+			}
+			else if (result == 1)
+			{
+				intervals[i] = std::make_pair(left, middle);
+				break;
+			}
+			else
+			{
+				left = middle;
+			}
+		}
+	}
+
+	return intervals;
 }
