a2379071e2cf16ad1be49d12ec1f59ba215b4f9f
123 <fomin.nv@phystech.edu>
2022-11-28 17:44:04 +0300

    copy constr and operator=

--- spline/Cell.cpp
+++ spline/Cell.cpp
@@ -12,3 +12,32 @@ Cell::~Cell()
 {
 	DestroyWindow(hWnd);
 }
+
+Cell::Cell(const Cell& other)
+{
+	hInst = other.hInst;
+	RECT rt;
+	TCHAR name[256];
+	GetWindowText(other.hWnd, name, 256);
+	GetWindowRect(other.hWnd, &rt);
+	MapWindowPoints(HWND_DESKTOP, GetParent(other.hWnd), reinterpret_cast<LPPOINT>(&rt), 2);
+	hWnd = CreateWindow(WC_EDIT, name, GetWindowLong(other.hWnd, GWL_STYLE), rt.left, rt.top,
+		rt.right - rt.left, rt.bottom - rt.top, GetParent(other.hWnd), GetMenu(other.hWnd), other.hInst, nullptr);
+	SetWindowLongPtr(hWnd, GWLP_USERDATA, GetWindowLongPtr(hWnd, GWLP_WNDPROC));
+	SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(WndProc));
+}
+
+Cell& Cell::operator=(const Cell& other)
+{
+	hInst = other.hInst;
+	RECT rt;
+	TCHAR name[256];
+	GetWindowText(other.hWnd, name, 256);
+	GetWindowRect(other.hWnd, &rt);
+	MapWindowPoints(HWND_DESKTOP, GetParent(other.hWnd), reinterpret_cast<LPPOINT>(&rt), 2);
+	hWnd = CreateWindow(WC_EDIT, name, GetWindowLong(other.hWnd, GWL_STYLE), rt.left, rt.top,
+		rt.right - rt.left, rt.bottom - rt.top, GetParent(other.hWnd), GetMenu(other.hWnd), other.hInst, nullptr);
+	SetWindowLongPtr(hWnd, GWLP_USERDATA, GetWindowLongPtr(hWnd, GWLP_WNDPROC));
+	SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(WndProc));
+	return *this;
+}
