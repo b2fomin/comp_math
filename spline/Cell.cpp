f39e718ae322c9987c7735226696ea574794ea65
123 <fomin.nv@phystech.edu>
2022-11-28 17:42:16 +0300

    Cell constr

---
+++ spline/Cell.cpp
@@ -0,0 +1,9 @@
+#include"Cell.h"
+
+Cell::Cell(HWND hWndParent, HINSTANCE hInst, int x, int y, int width, int height)
+	: hWnd{ CreateWindow(WC_EDIT, nullptr, WS_CHILD | WS_VISIBLE | WS_DLGFRAME | ES_NOHIDESEL, x,
+		y, width, height, hWndParent, nullptr, hInst, nullptr) }, hInst{ hInst }
+{
+	SetWindowLongPtr(hWnd, GWLP_USERDATA, GetWindowLongPtr(hWnd, GWLP_WNDPROC));
+	SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(WndProc));
+}
