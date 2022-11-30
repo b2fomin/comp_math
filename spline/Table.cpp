f534d9c3b2e47ab465798e7658ece52ea2b27b6c
123 <fomin.nv@phystech.edu>
2022-11-30 05:29:45 +0300

    copy constr and operator=

--- spline/Table.cpp
+++ spline/Table.cpp
@@ -53,3 +53,57 @@ Table::~Table()
 	DestroyWindow(hWnd);
 	if (!--count) UnregisterClass(_ClassName, hInst);
 }
+
+Table::Table(const Table& other)
+{
+	RECT rt;
+	TCHAR name[256];
+	GetWindowText(other.hWnd, name, 256);
+	GetWindowRect(other.hWnd, &rt);
+	MapWindowPoints(HWND_DESKTOP, GetParent(other.hWnd), reinterpret_cast<LPPOINT>(&rt), 2);
+	hWnd = CreateWindow(_ClassName, name, GetWindowLong(other.hWnd, GWL_STYLE), rt.left, rt.top,
+		rt.right - rt.left, rt.bottom - rt.top, GetParent(other.hWnd), GetMenu(other.hWnd), other.hInst, this);
+	SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
+	hInst = other.hInst;
+	cells = other.cells;
+	for (int i = 0; i < cells.size(); ++i)
+		for (int j = 0; j < cells[i].size(); ++j)
+		{
+			GetWindowRect(other.cells[i][j].GetHWND(), &rt);
+			MapWindowPoints(HWND_DESKTOP, other.hWnd, reinterpret_cast<LPPOINT>(&rt), 2);
+			SetParent(cells[i][j].GetHWND(), hWnd);
+			MoveWindow(cells[i][j].GetHWND(), rt.left, rt.top, rt.right - rt.left, rt.bottom - rt.top, TRUE);
+		}
+	cell_width = other.cell_width;
+	cell_height = other.cell_height;
+	focused_cell = other.focused_cell;
+	SetFocus(cells[focused_cell.first][focused_cell.second].GetHWND());
+}
+
+Table& Table::operator=(const Table& other)
+{
+	RECT rt;
+	TCHAR name[256];
+	GetWindowText(other.hWnd, name, 256);
+	GetWindowRect(other.hWnd, &rt);
+	MapWindowPoints(HWND_DESKTOP, GetParent(other.hWnd), reinterpret_cast<LPPOINT>(&rt), 2);
+	hWnd = CreateWindow(_ClassName, name, GetWindowLong(other.hWnd, GWL_STYLE), rt.left, rt.top,
+		rt.right - rt.left, rt.bottom - rt.top, GetParent(other.hWnd), GetMenu(other.hWnd), other.hInst, this);
+	SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
+	hInst = other.hInst;
+	cells = other.cells;
+	for (int i = 0; i < cells.size(); ++i)
+		for (int j = 0; j < cells[i].size(); ++j)
+		{
+			GetWindowRect(other.cells[i][j].GetHWND(), &rt);
+			MapWindowPoints(HWND_DESKTOP, other.hWnd, reinterpret_cast<LPPOINT>(&rt), 2);
+			SetParent(cells[i][j].GetHWND(), hWnd);
+			MoveWindow(cells[i][j].GetHWND(), rt.left, rt.top, rt.right - rt.left, rt.bottom - rt.top, TRUE);
+		}
+	cell_width = other.cell_width;
+	cell_height = other.cell_height;
+	focused_cell = other.focused_cell;
+	SetFocus(cells[focused_cell.first][focused_cell.second].GetHWND());
+	return *this;
+}
+
