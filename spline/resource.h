9d60e9c4edad03b655bdb0641ddf2ac7f33a2081
123 <fomin.nv@phystech.edu>
2022-11-30 05:58:41 +0300

    resource files

---
+++ spline/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется spline.rc
+//
+#define IDR_MENU1                       102
+#define IDM_GRAPH                       40001
+#define IDM_CALC                        40002
+#define IDM_CLEAR                       40004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
