460417ab4f359a8aaa3a7e1ea028e880ddf1b517
123 <fomin.nv@phystech.edu>
2022-11-30 05:39:55 +0300

    constr and destr

--- spline/Graph.cpp
+++ spline/Graph.cpp
@@ -20,3 +20,21 @@ bool Graph::InitWndClass()
 	}
 	else return true;
 }
+
+Graph::Graph(LPCTSTR lpWindowName, DWORD dwStyle, HWND hWndParent, HINSTANCE hInst, int x, int y, int width, int height, int ticksX, int ticksY)
+	: hInst{ hInst }, GraphSize{ 1200 }, GraphWidth{ 1000 }, ticksX{ ticksX }, ticksY{ ticksY }, indent{ 25 },
+	min_x{ INFINITY }, max_x{ -INFINITY }, min_y{ INFINITY }, max_y{ -INFINITY }, hline{ CreatePen(PS_SOLID, 6, RGB(0, 0, 255)) }
+{
+	InitWndClass();
+	++count;
+	hWnd = CreateWindow(_ClassName, lpWindowName, dwStyle,
+		x, y, width, height, hWndParent, nullptr, hInst, this);
+	SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
+}
+
+Graph::~Graph()
+{
+	DeleteObject(hline);
+	DestroyWindow(hWnd);
+	if (!--count) UnregisterClass(_ClassName, hInst);
+};
