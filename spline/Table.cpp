67ac99ce4cc073a3a2cae52c841ed4930bbe0359
123 <fomin.nv@phystech.edu>
2022-11-30 05:29:09 +0300

    constr and destr

--- spline/Table.cpp
+++ spline/Table.cpp
@@ -19,3 +19,37 @@ bool Table::InitWndClass()
 	}
 	return true;
 }
+
+Table::Table
+(
+	LPCWSTR lpWindowName,
+	DWORD dwStyle,
+	int X,
+	int Y,
+	int nWidth,
+	int nHeight,
+	HWND hWndParent,
+	HMENU hMenu,
+	HINSTANCE hInstance,
+	int rows_number, int cols_number
+) :
+	hInst{ hInstance }
+{
+	InitWndClass();
+	++count;
+	hWnd = CreateWindow(_ClassName, lpWindowName, dwStyle, X, Y, nWidth, nHeight,
+		hWndParent, hMenu, hInstance, this);
+	SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
+	cells.push_back(std::vector<Cell>(0));
+	for (int i = 0; i < cols_number; ++i) AddColumn();
+	for (int i = 0; i < rows_number; ++i) AddRow();
+	focused_cell = std::make_pair(0, 0);
+	SetFocus(cells[0][0].GetHWND());
+}
+
+Table::~Table()
+{
+	cells.clear();
+	DestroyWindow(hWnd);
+	if (!--count) UnregisterClass(_ClassName, hInst);
+}
