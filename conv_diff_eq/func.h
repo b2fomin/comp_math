f8be772da26a2ebd88bcbfb4f78197eadd81dc60
unknown <fomin.nv@phystech.edu>
2023-04-11 17:34:34 +0300

    initial functions and derivatives

---
+++ conv_diff_eq/func.h
@@ -0,0 +1,62 @@
+#pragma once
+#include<cmath>
+
+double a(double x, double t)
+{
+	return -1;
+}
+
+double b(double x, double t)
+{
+	return t;
+}
+
+double varphi(double x)
+{
+	return x + 1;
+}
+
+double psi(double t)
+{
+	return std::exp(t) + t + 1;
+}
+template<typename T>
+auto der_1st(T f, double dx)
+{
+	return [f, dx](double x)
+	{return (f(x + dx) - f(x - dx)) / (2 * dx); };
+}
+
+template<typename T>
+auto der_2nd(T f, double dx)
+{
+	return [f, dx](double x)
+	{return (f(x + dx) + f(x - dx) - 2 * f(x)) / (2 * dx * dx); };
+}
+
+auto a_t(double x, double t, double dt)
+{
+	return der_1st([x, dt](double t) {return a(x, t); }, dt)(t);
+}
+auto a_x(double x, double t, double dx)
+{
+	return der_1st([t, dx](double x) {return a(x, t); }, dx)(x);
+}
+
+auto b_t(double x, double t, double dt)
+{
+	return der_1st([x, dt](double t) {return b(x, t); }, dt)(t);
+}
+auto b_x(double x, double t, double dx)
+{
+	return der_1st([t, dx](double x) {return b(x, t); }, dx)(x);
+}
+
+auto psi_1st(double t, double dt)
+{
+	return der_1st(psi, dt)(t);
+}
+auto psi_2nd(double t, double dt)
+{
+	return der_2nd(psi, dt)(t);
+}
